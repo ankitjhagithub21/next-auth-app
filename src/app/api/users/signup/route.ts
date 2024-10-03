d9e69543714bc53174bbccf2794ab3a2fcc15bb9
ankitjhagithub21 <ajha45075@gmail.com>
2024-10-03 21:24:53 +0530

    created login and sign up page

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -11,7 +11,6 @@ export async function POST(request:NextRequest) {
         const reqBody = await request.json()
         const {username,email,password} = reqBody;
 
-
         const user = await User.findOne({email})
 
         if(user){
